c301300be6d642e747dd76da52fe732868e0ee6a
nepallium <122187419+nepallium@users.noreply.github.com>
2025-09-21 20:46:43 -0400

    add waveform

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,17 @@
+import Waveform from "./Waveform";
+
 export default function App() {
+  // const url = "/test_audio.mp3";
+  const url = "https://on.soundcloud.com/8SABMWyuxzTDAhpCf3";
+
   return (
-    <h1 className="text-3xl font-bold underline flex justify-center items-center h-screen">
-      Hello world!
-    </h1>
+    <>
+      <h1 className="text-3xl font-bold underline flex justify-center">
+        Hello world!
+      </h1>
+
+      <Waveform audioUrl={url} className="w-4 h-10" />
+    </>
+
   )
 }
